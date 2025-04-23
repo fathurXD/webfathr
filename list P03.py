82f8b52ec8a0cb3b654375bf679b9f5ea9885c70
FathurXD <raflifthr@gmail.com>
2025-04-23 21:07:26 +0700

    First commit

---
+++ list P03.py
@@ -0,0 +1,14 @@
+buah = ["apel", "mangga", "pisang"]
+print(buah[0])
+print(buah[1])
+print(buah[2])
+
+buah.remove("apel")
+print(buah)
+
+buah.append("apel")
+print(buah)
+
+buah.remove("apel")
+buah.insert(0,"nanas")
+print(buah)
